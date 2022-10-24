7927fce3b3f905b56f323585f857aa3759618fda
artemiygusev04 <PMPO1002@yandex.ru>
2022-10-25 00:11:33 +0300

    test1

--- base_page/pages/pick_point.py
+++ base_page/pages/pick_point.py
@@ -1,4 +1,3 @@
-import time
 from selene import be
 from selene.support.shared.jquery_style import s, ss
 from selene import query
@@ -9,21 +8,14 @@ from base_page.helper.acceptance_test_modul import click_with_offset
 
 
 def select_pick_point():
-    # i = 0
-    # while i <= 5:
-    #     i += 1
-    #     time.sleep(1)
-    #     if s('//ymaps[@class="ymaps-2-1-79-svg-icon-content"]/ymaps').matching(be.visible):
-    #         s('[class*="ZoomButtons_plus"]').click()
-    #     else:
-    #         break
-
-    #x_y = ss('#yandex-map-id > ymaps > ymaps > ymaps > ymaps.ymaps-2-1-79-places-pane > ymaps')
-    x_y = browser.executeScript("document.querySelectorAll('#yandex-map-id > ymaps > ymaps > ymaps > ymaps.ymaps-2-1-79-places-pane > ymaps').length")
-    while x_y == 1:
-        x_y -= 1
-        x_y = s('#yandex-map-id > ymaps > ymaps > ymaps > ymaps.ymaps-2-1-79-places-pane > ymaps:nth-child(' + x_y + ')')
-
+    s('[class*="Pickup_selectBtn"]').click()
+    browser.execute_script('return document.readyState == "complete"')
+    s('[class*="ZoomButtons_plus"]').click()
+    i = 0
+    while i <= 20:
+        i += 1
+        x_y = s('#yandex-map-id > ymaps > ymaps > ymaps > ymaps.ymaps-2-1-79-places-pane > ymaps:nth-child(' + str(i) + ')')
+        int(i)
         x_y = x_y.get(query.attribute('style'))
         regex = r"^.*left\:\s(.\d+).*top\:\s(.\d+).*$"
 
@@ -31,14 +23,8 @@ def select_pick_point():
         x = int(matches.groups()[0]) + -960
         y = int(matches.groups()[1]) + -403
 
-    # while i <= 10:
-    #     i += 1
-    #    if s('[class="primary-button js-select-point"]').matching(be.not_.visible):
         s('#yandex-map-id').perform(click_with_offset(x, y))
-        if s('[class="primary-button js-select-point"]').should(be.visible):
+
+        if s('[class="primary-button js-select-point"]').matching(be.visible):
+            s('[class="primary-button js-select-point"]').click()
             break
-        # else:
-        #     break
-    #s('[class="primary-button js-select-point"]').click()
-    #s('#__next__checkout > div > div > div.Checkout_cols__xDKnD > div.Checkout_stepWrapper__9rLsG > div.ShippingInfo_root__WvOS5 > form > div:nth-child(4) > div.BlockTitle_root__yWK3w').should(be.visible)
-    #time.sleep(10)
--- tests/test_acceptance/test_account.py
+++ tests/test_acceptance/test_account.py
@@ -1,13 +1,11 @@
-import time
-
-from base_page.helper.acceptance_test_modul import url_open_size, add_file, remove_element, passing_modal, scroll_click
-from base_page.controls.application_manager import app
+from base_page.helper.acceptance_test_modul import url_open_size, passing_modal, scroll_click
 from env import *
 import allure
 from allure_commons.types import Severity
 from selene.support.shared.jquery_style import s, ss
 from base_page.pages.pick_point import select_pick_point
 from base_page.pages.change_city import change_city
+from selene import be
 
 
 def test_case_practice_form():
@@ -19,42 +17,22 @@ def test_case_practice_form():
     with allure.step('Открываем /automation-practice-form'):
         url_open_size('/internet-magazin/devices')
         passing_modal()
-
-    with allure.step('Заполняем поля данными'):
-        scroll_click(add_device)
-
-        s(go_to_checkout).click()
-
         change_city()
 
+    with allure.step('Добавляем девайс в корзину'):
+        scroll_click(add_device)
+
+    with allure.step('Переходим на чекаут'):
+        s(go_to_checkout).click()
+
+    with allure.step('Добавляем еще один девайс в корзину'):
+        s('[alt="plus"]').click()
+
+    with allure.step('Нажимаем на выбрать пункт выдачи'):
         s(delivery_pick_point).click()
 
-        s(select_point).click()
-
+    with allure.step('Выбираем ПикПоинт'):
         select_pick_point()
 
-
-
-
-
-        #s('//*[@id="userEmail"]').type('Jack@mail.ru')
-        #s('//*[@id="userNumber"]').type('4815162342')
-        #s('//*[@id="currentAddress"]').type('Oceanic')
-
-    #with allure.step('Выбираем элемент в поле Subject'):
-        #app.subject(s(subjects_input)).select_element_in_list('g', select_element_in_subject)
-
-    #with allure.step('Выбираем текущую дату'):
-    #    app.date_picker(s(date_of_birth_input)).select_date_in_datepicker(date_of_birth)
-
-    #with allure.step('Выбираем пол: male'):
-    #    s(gender_select_male).click()
-
-    #with allure.step('Выбираем хобби: Спорт'):
-    #    s(hobbies_select_sports).click()
-
-    #with allure.step('Добавляем картинку в поле загрузки файла'):
-    #    add_file(send_picture_button, file_name=file_name1)
-
-    #with allure.step('Кликаем по кнопки "Отправить форму"'):
-    #    s(send_data).click()
+    with allure.step('Проверяем что заказ доступен только зарегистрированным пользователям'):
+        s('//*[text()="Заказ доступен только зарегистрированным пользователям."]').should(be.visible)
